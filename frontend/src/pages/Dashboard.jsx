f0f05c011edc827b071ece4b66e3964399c4168e
unknown <catovicnaida979@gmail.com>
2025-06-17 10:34:01 +0200

    V1 verzija: Inicijalni projekat

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Dobrodošli na Dashboard</h1>
+      <p>Ovo je početna stranica vaše aplikacije.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
