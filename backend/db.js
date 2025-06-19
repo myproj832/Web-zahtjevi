c15d39d7bda2311cb01dd3ad3fade227e7707d3c
unknown <catovicnaida979@gmail.com>
2025-06-19 15:10:30 +0200

    Dodani API servisi za prijavu

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
 const oracledb = require('oracledb');
 
-async function getConnection() {
-  return await oracledb.getConnection({
+const dbConfig = {
     user: 'pro_zahtjevi',
     password: 'pro_zahtjevi',
     connectionString: '192.168.100.1:1521/finc'
-  });
+};
+
+async function getConnection() {
+  return await oracledb.getConnection(dbConfig);
 }
 
 module.exports = { getConnection };
