4af913f0049d759b71fed1e695c28d8c10562abe
unknown <catovicnaida979@gmail.com>
2025-06-23 12:24:11 +0200

    Servisi napravljeni i podignuti dodat cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const loginRoute = require('./routes/login');
 
 const app = express();
 const PORT = 3334
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', loginRoute);
 
