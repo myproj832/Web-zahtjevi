0c94d0036553fec9801b15471b2a21bd6838fbc2
MakiLe <marijalekic92@gmail.com>
2025-07-01 11:25:46 +0200

    Fix: Data Context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,13 +6,16 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { DataProvider } from './context/DataContext.jsx';
 
 createRoot(document.getElementById('root')).render(
  
   <React.StrictMode>
    <BrowserRouter>
       <AuthProvider>
+        <DataProvider>
         <App />
+        </DataProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
